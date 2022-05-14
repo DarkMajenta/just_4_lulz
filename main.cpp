9535fc3d8cb78bda586b7f90250d444b53a87db4
Vrl_mck <mackofee-328@yandex.com>
2022-05-14 18:35:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 //         https://www.youtube.com/channel/UCu60M9W_SX-SNo53CDNVJ4w
 //         https://github.com/reo7sp/tgbot-cpp
 //         https://stackoverflow.com/questions/36702888/download-an-image-from-an-url-using-curl-in-c
+//         https://stackoverflow.com/questions/36693792/text-parser-c-code#36696232
+//         https://stackoverflow.com/questions/46261431/parsing-through-text-file-with-c
+//         https://stackoverflow.com/questions/9551014/reading-parsing-text-file-input-c
+//         https://www.cplusplus.com/forum/beginner/879/
 #include <iostream>
 #include <stdio.h>
 #include <tgbot/tgbot.h>
